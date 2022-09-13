8c5d0806fd6ebadf6787db222f29e2428a69f4ca
dukemiller <dukemiller928@hotmail.com>
2022-09-13 13:16:20 -0700

    Fixed to work with latest version of youtube; fixed macos cmd-click issue

--- content-script.js
+++ content-script.js
@@ -1,82 +1,68 @@
-// Change the href of a node to have '/videos' concatenated on it
-addToHref = (node) => {
-    const href = node.getAttribute("href");
-    if (href.indexOf("videos") === -1)
-        node.setAttribute("href", href + "/videos");
+const VIDEO_CONTAINER = "div#owner.item.style-scope.ytd-watch-metadata"
+const VIDEO_METADATA = "YTD-WATCH-FLEXY"
+const VIDEO_ID_ATTR = "video-id"
+const WAIT_DELAY = 1000
+const WAIT_MAX_ATTEMPTS = 30
+
+// Change href to have '/videos' concatenated at end
+const updateHref = (node) => {
+    const href = node.getAttribute("href")
+    if (href != null && !href.includes("videos")) {
+        node.setAttribute("href", `${href}/videos`);
+    }
 }
 
-// Stop any SPA event handlers and instead, go to the intended page
-changeOnClick = (node) => {
+// Stop any SPA event handlers and go to intended page
+const overrideOnClick = (node) => {
     node.addEventListener("click", e => {
-        if (e.ctrlKey)
-            return;
+        if (e.ctrlKey || e.metaKey) return;
         e.cancelBubble = true;
         e.preventDefault();
         e.stopPropagation();
-
         browser.runtime.sendMessage({
             url: "https://www.youtube.com" + node.getAttribute("href")
         });
-
     }, true);
 }
 
-// Constantly observe the video url node for changes, update when needed
-observeVideoNode = (node) => {
-    addToHref(node);
-    changeOnClick(node);
-
-    const observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-            addToHref(node);
-            changeOnClick(node);
-        });
-    });
-
-    observer.observe(node, {
-        attributes: true
-    });
-}
-
-// Determine if the node is the correct node we need
-isCorrectNode = (node) => {
-    return node.nodeName === "A" &&
-        node.getAttribute("href") != null &&
-        node.getAttribute("href").indexOf("channel/") == 1;
-}
-
-// Check for any changes in DOM tree until the one we want exists
-observeUntilVideoNodeExists = () => {
-    const observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-            if (location.href.indexOf("watch?") === -1 || !mutation.addedNodes)
-                return;
-
-            for (var i = 0; i < mutation.addedNodes.length; i++) {
-                var node = mutation.addedNodes[i]
-                if (isCorrectNode(node)) {
-                    observeVideoNode(node);
-                    observer.disconnect();
-                }
-            }
-        })
-    });
-
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-        attributes: false,
-        characterData: false
+// Change all links related to the content owner
+const updateVideoOwnerDetails = (node) => {
+    node.querySelectorAll("a[href]").forEach((a) => {
+        updateHref(a);
+        overrideOnClick(a);
     })
 }
 
-// Main
-(function(){
-    const node = document.querySelector("#owner-name > a:nth-child(1)");
+// Change details and then observe changes
+const observePageChanges = (node) => {
+    updateVideoOwnerDetails(node)
 
-    // Loaded a page that isn't a video
-    if (node == null)
-        observeUntilVideoNodeExists();
-    else
-        observeVideoNode(node);
-})()
+    const observer = new MutationObserver((mutations) => {
+        for (var mutation of mutations) {
+            if (mutation.attributeName === VIDEO_ID_ATTR) {
+                updateVideoOwnerDetails(node)
+            }
+        }
+    });
+
+    observer.observe(
+        document.querySelector(VIDEO_METADATA),
+        { attributeFilter: [VIDEO_ID_ATTR] }
+    );
+}
+
+// Wait until the element exists and resolve
+const waitForElement = (selector) => {
+    var attempt = 0
+    return new Promise((resolve, reject) => {
+        const intervalId = setInterval(() => {
+            var el = document.querySelector(selector)
+            if (el) { clearInterval(intervalId); resolve(el); }
+            else if (attempt >= WAIT_MAX_ATTEMPTS) { reject() }
+            else { attempt++ }
+        }, WAIT_DELAY);
+    });
+}
+
+// Main
+waitForElement(VIDEO_CONTAINER).then(observePageChanges)
